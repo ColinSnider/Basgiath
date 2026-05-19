2ed3df1ce9028d3195c64f268e8b97e302ce4c96
Colin Snider <123518273+ColinSnider@users.noreply.github.com>
2026-05-18 21:07:21 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     tailwindcss(),
     tsConfigPaths({ projects: ["./tsconfig.json"] }),
     tanstackStart({
-      server: { entry: "server" },
+      server: { entry: "index" },
       importProtection: {
         behavior: "error",
         client: {
